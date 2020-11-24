f72b3f8c4dcb795ef85e109a57bebecb70e434ec
Remigijus <remigijus@vizualiterapija.lt>
2020-11-24 13:56:09 +0200

    Initial commit

---
+++ Calculator01/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Calculator Layout iOS13
+//
+//  Created by Remigijus Klimovas with a help from Angela Yu on 2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
